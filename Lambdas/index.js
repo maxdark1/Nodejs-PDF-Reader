c4d86926bb49421b6c5054c7390dee62057dd994
Juan Lara <max_lara3005@hotmail.com>
2023-06-28 07:59:51 -0600

    Adding S3 Bucket Integration

--- Lambdas/index.js
+++ Lambdas/index.js
@@ -4,6 +4,7 @@ const require = createRequire(import.meta.url);
 
 const pdfParse = require('pdf-parse');
 const fs = require('fs');
+const AWS = require('aws-sdk');
 
 const getText = async (file) => {
     let readFileSync = fs.readFileSync(file);
@@ -28,9 +29,18 @@ const getImg = async (file) => {
     );
 }
 
+const getS3BucketFile = async (file) => {
+    const s3 = new AWS.S3();ß
+    const params = {Bucket: 'NeorisBucket', Key: file};
+    const response = await s3.getObject(params).promise();
+}
 
 const pdftoRead = '../Patito_Feo.pdf';
+/*Get the file from the local file System*/
 await getText(pdftoRead);
 await getImg(pdftoRead);
+/*Get the file from S3 Bucket*/
+await getText(getS3BucketFile);
+await getImg(getS3BucketFile);
 
 
